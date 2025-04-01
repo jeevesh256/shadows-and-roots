ce4232410821149a50795c9bf453fadbcf03a246
Jeevesh B <119961922+jelly-bea@users.noreply.github.com>
2025-04-01 17:46:58 +0530

    v6.52

--- scripts/breakable_wall.gd
+++ scripts/breakable_wall.gd
@@ -1,9 +1,16 @@
 extends StaticBody2D
 
 var attack_collision_count = 0
+var brk = false
 
 func _on_wall_collision_area_entered(area):
-	if area.name == "attack_collision":
+	if area.name == "attack_collision" and brk:
 		attack_collision_count += 1
 		if attack_collision_count >= 3:
 			queue_free()
+
+
+
+func _on_wall_collision_body_entered(body):
+	if body.name == "player":
+		brk = true
